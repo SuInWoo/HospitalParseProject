fbcb2afc729da6fe3e035ed61e4a147e57699495
SuInWoo <wbo1026@naver.com>
2022-10-13 13:21:02 +0900

    "를 없에는 부분 추가

--- src/main/java/com/parser/HospitalParser.java
+++ src/main/java/com/parser/HospitalParser.java
@@ -5,6 +5,8 @@ import com.domain.Hospital;
 public class HospitalParser implements Parser<Hospital>{
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"", ""); //" 없에기
+
         String[] splitted = str.split(",");
         return new Hospital(splitted[0], splitted[1], splitted[1], splitted[2],
                 splitted[6], splitted[10], splitted[3]);
