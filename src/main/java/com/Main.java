1346b5a8381940bff0e3e41d7342c282edd706e6
SuInWoo <wbo1026@naver.com>
2022-10-13 12:47:40 +0900

    서울시 병의원 위치 Parser(Test수정)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -12,7 +12,6 @@ public class Main {
         String filename = "/Users/suin/Downloads/수업 데이터 파일/seoul_hospital.csv";
         List<Hospital> hospitals = hospitalLineReader.readLines(filename);
 
-
         for (Hospital hospital : hospitals) {
             System.out.printf("%s %s %s %s %s %s %s\n",
                     hospital.getId(), hospital.getAddress(), hospital.getDistrict(), hospital.getCategory(),
