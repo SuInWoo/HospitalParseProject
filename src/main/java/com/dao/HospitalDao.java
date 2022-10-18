eeea53e846e3e087c5c947a997536749758e0a33
SuInWoo <wbo1026@naver.com>
2022-10-18 10:48:08 +0900

    Hospital connect Hospital(Connection 분리)

--- src/main/java/com/dao/HospitalDao.java
+++ src/main/java/com/dao/HospitalDao.java
@@ -21,8 +21,9 @@ public class HospitalDao {
 
     public void add() throws SQLException, ClassNotFoundException {
         Connection conn = getConnection();
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, address, district, category, emergency_room, name, subdivision) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, address, district, category, " +
+                "emergency_room, name, subdivision VALUES (?, ?, ?, ?, ?, ?, ?)");
         ps.setString(1, "");
         ps.setString(2, "");
         ps.setString(3, "");
@@ -38,13 +39,17 @@ public class HospitalDao {
 
     public Hospital get(String id) throws ClassNotFoundException, SQLException {
         Connection conn = getConnection();
+
         PreparedStatement ps = conn.prepareStatement("SELECT id, address, district, category, emergency_room," +
                 " name, subdivision FROM `seoul_hospital` WHERE id = ?");
+
         ps.setString(1, id);
         ResultSet rs = ps.executeQuery();
         rs.next();
-        Hospital hospital = new Hospital(rs.getString("id"), rs.getString("address"), rs.getString("district"),
-                rs.getString("category"), rs.getString("emergency_room"), rs.getString("name"), rs.getString("subdivision"));
+        Hospital hospital = new Hospital(rs.getString("id"), rs.getString("address"),
+                rs.getString("district"), rs.getString("category"),
+                rs.getString("emergency_room"), rs.getString("name"),
+                rs.getString("subdivision"));
 
         rs.close();
         ps.close();
@@ -58,7 +63,8 @@ public class HospitalDao {
 //            hospital.add();
         Hospital hospital = hospitalDao.get("A1120837");
         System.out.printf("#id: %s\n#address: %s\n#district: %s\n#category: %s\n#emergency_room: %s\n#name: %s\n#subdivision: %s\n",
-                hospital.getId(), hospital.getAddress(), hospital.getDistrict(), hospital.getCategory(), hospital.getEmergencyRoom(),
-                hospital.getName(), hospital.getSubdivision());
+                hospital.getId(), hospital.getAddress(), hospital.getDistrict(),
+                hospital.getCategory(), hospital.getEmergencyRoom(), hospital.getName(),
+                hospital.getSubdivision());
     }
 }
