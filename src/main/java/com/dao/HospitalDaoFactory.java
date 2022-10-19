a5d1e1a9a39531387786c47a68632195c0c4a85c
SuInWoo <wbo1026@naver.com>
2022-10-19 13:44:03 +0900

    spring dependency 추가 - 어노테이션 추가 및 테스트 실행

--- src/main/java/com/dao/HospitalDaoFactory.java
+++ src/main/java/com/dao/HospitalDaoFactory.java
@@ -1,13 +1,20 @@
 package com.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class HospitalDaoFactory {
     //조립 역할
-    public HospitalDao awsUserDao() {   //날개 5개 선풍기
+
+    @Bean
+    public HospitalDao awsHospitalDao() {   //날개 5개 선풍기
         HospitalDao hospitalDao = new HospitalDao(new AWSConnectionMaker());
         return hospitalDao;
     }
 
-    public HospitalDao localUserDao() { //날개 4개 선풍기
+    @Bean
+    public HospitalDao localHospitalDao() { //날개 4개 선풍기
         HospitalDao hospitalDao = new HospitalDao(new LocalConnectionMaker());
         return hospitalDao;
     }
--- src/test/java/com/dao/HospitalDaoTest.java
+++ src/test/java/com/dao/HospitalDaoTest.java
@@ -1,26 +1,36 @@
 package com.dao;
 
 import com.domain.Hospital;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HospitalDaoFactory.class)
 class HospitalDaoTest {
 
-    String case1 = "\"A1120837\", \"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\n" +
-            "\"서울특별시 금천구\", \"C\", \"2\", \"가산기대찬의원\", \"분류 불가\"";
+    @Autowired
+    ApplicationContext context;
 
     @Test
     @DisplayName("Select Test")
     void select() throws SQLException {
-//        HospitalDao hospitalDao = new HospitalDao(AWSConnectionMaker);
 
-        HospitalDao hospitalDao = new HospitalDaoFactory().awsUserDao();
+        HospitalDao hospitalDao = context.getBean("awsHospitalDao", HospitalDao.class);
+        String case1 = "\"A1120837\", \"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\",\n" +
+                "\"서울특별시 금천구\", \"C\", \"2\", \"가산기대찬의원\", \"분류 불가\"";
+
         Hospital hospital = hospitalDao.get("A1120837");
-
-        assertEquals("가산기대찬의원", hospital.getName());
+        Assertions.assertEquals("가산기대찬의원", hospital.getName());
     }
 }
